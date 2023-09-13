2176a893f2ddf68b797a40a7f03bd3c0a8c716ad
Heitor Martins <131913857+Keniche46@users.noreply.github.com>
2023-09-13 09:48:22 -0300

    Ke.py

--- Ke.py
+++ Ke.py
@@ -1,13 +1,49 @@
-nome = input('digite seu nome: ').lower()
-
-lista_de_palavras = nome.split()
-
-for palavra in lista_de_palavras:
-    meia_palavra = palavra [1:-1]
-    ultima_letra = palavra [-1]
-    primera_letra = palavra [0]
-    primera_letra_maiuscula = primera_letra.upper()
-
-    palavra_final = primera_letra_maiuscula + meia_palavra + ultima_letra
-
-    print(palavra_final)
+import random 
+import time
+  
+def ganhar(user, random): 
+     if user == random: 
+         print(f'\nUser: {user}\nMáquina: {random}') 
+         print('Empate\n')
+         time.sleep(0.7)
+     elif user == 'Pedra' and random == 'Tesoura': 
+         print(f'\nUser: {user}\nMáquina: {random}') 
+         print('Você ganhou!\n')
+         time.sleep(0.7)
+     elif user == 'Papel' and random == 'Pedra':
+         print(f'\nUser: {user}\nMáquina: {random}')
+         print('Você ganhou!\n')
+         time.sleep(0.7)
+     elif user == 'Tesoura' and random == 'Papel':
+         print(f'\nUser: {user}\nMáquina: {random}')
+         print('Você ganhou!\n')
+         time.sleep(0.7)
+     elif user == 'Tesoura' and random == 'Pedra':
+         print(f'\nUser: {user}\nMáquina: {random}')
+         print('Você perdeu!\n')
+         time.sleep(0.7)
+     elif user == 'Papel' and random == 'Tesoura':
+         print(f'\nUser: {user}\nMáquina: {random}')
+         print('Você perdeu!\n')
+         time.sleep(0.7)
+     elif user == 'Pedra' and random == 'Papel':
+         print(f'\nUser: {user}\nMáquina: {random}')
+         print('Você perdeu!\n')
+         time.sleep(0.7)
+def menu(): 
+     print('Escolha a opcão que deseja jogar.') 
+     print('[1]- Pedra') 
+     print('[2]- Papel') 
+     print('[3]- Tesoura') 
+     print('[0]- Sair') 
+  
+escolhas_jogo = ['Pedra','Papel','Tesoura'] 
+  
+while True: 
+     menu() 
+     opção = int(input('Insira o número referente sua escolha: ')) 
+     if opção == 0: 
+         break 
+     escolha_usuario = escolhas_jogo[opção - 1] 
+     escolha_random = random.choice(escolhas_jogo) 
+     ganhar(escolha_usuario, escolha_random)
